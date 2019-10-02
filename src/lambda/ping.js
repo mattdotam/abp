001a46ab79d49acab477640824b272882a32d785
Matt Bear <hi@matt.am>
2019-10-02 15:25:58 +1000

    rename lambda to functions

--- src/lambda/ping.js
+++ src/lambda/ping.js
@@ -1,23 +1,16 @@
-'use strict';
 const express = require("express");
-const path = require("path");
 const serverless = require('serverless-http');
 const cors = require('cors')
 const app = express();
 const bodyParser = require("body-parser");
 
-const router = express.Router();
-
-router.get('/ping', (req, res) => {
-  res.json("pong!");
-});
-
 app.use(cors());
 app.use(express.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
-app.use("/.netlify/lambda/ping", router);
-app.use("/", (req, res) => res.sendFile(path.join(__dirname, "../build/index.html")));
+app.get('/ping', (req, res) => {
+  res.json("pong!");
+});
 
 module.exports = app;
 module.exports.handler = serverless(app);
