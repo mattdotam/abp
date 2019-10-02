d63cc390edbf6d50f19a7bde93f995018582aff2
Matt Bear <hi@matt.am>
2019-10-02 14:06:46 +1000

    add relative path for index from express app

--- src/lambda/ping.js
+++ src/lambda/ping.js
@@ -1,17 +1,23 @@
 'use strict';
-const serverless = require('serverless-http');
 const express = require("express");
+const path = require("path");
+const serverless = require('serverless-http');
 const cors = require('cors')
 const app = express();
 const bodyParser = require("body-parser");
+
+const router = express.Router();
+
+router.get('/ping', (req, res) => {
+  res.json("pong!");
+});
 
 app.use(cors());
 app.use(express.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
-app.get('/ping', (req, res) => {
-  res.json("pong!");
-});
+app.use("/.netlify/lambda/ping", router);
+app.use("/", (req, res) => res.sendFile(path.join(__dirname, "../build/index.html")));
 
 module.exports = app;
 module.exports.handler = serverless(app);
