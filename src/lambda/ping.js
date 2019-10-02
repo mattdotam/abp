75de57f32cb6b63cb41d8d37248efa3b86499658
Matt Bear <hi@matt.am>
2019-10-02 16:01:07 +1000

    trying new netlify config, reducing dependencies in ping

--- src/lambda/ping.js
+++ src/lambda/ping.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const serverless = require('serverless-http');
-const cors = require('cors')
+// const cors = require('cors')
 const app = express();
-const bodyParser = require("body-parser");
+// const bodyParser = require("body-parser");
 
-app.use(cors());
-app.use(express.urlencoded({extended:true}));
-app.use(bodyParser.json());
+// app.use(cors());
+// app.use(express.urlencoded({extended:true}));
+// app.use(bodyParser.json());
 
 app.get('/ping', (req, res) => {
-  res.json("pong!");
+  res.json({"foo": "bar"});
 });
 
 module.exports = app;
