483f0ff93e24b15bc9887cad7b984ddee81a65b5
Matt Bear <hi@matt.am>
2019-10-02 20:06:17 +1000

    add Browser component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,11 @@
 import React from "react";
-import { Grid, Typography } from "@material-ui/core";
+import { Grid } from "@material-ui/core";
+import Browser from "./Browser";
 
 const Home = () => {
   return (
-    <Grid direction="column" container>
-      <Grid>
-        <Typography variant="h1" component="h1">
-          Home
-        </Typography>
-      </Grid>
+    <Grid direction="column">
+      <Browser />
     </Grid>
   );
 };
